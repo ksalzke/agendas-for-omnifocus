77f1a17fec8675af772b3b9292d274fb4881b165
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2022-02-07 12:22:42 +1100

    add preference for linked event tag

--- Agendas.omnifocusjs/Resources/preferences.js
+++ Agendas.omnifocusjs/Resources/preferences.js
@@ -7,17 +7,20 @@
     const itemTag = this.agendasLibrary.prefTag('itemTag')
     const addToNote = (syncedPrefs.read('addToNote') !== null) ? syncedPrefs.readBoolean('addToNote') : true
     const eventTags = this.agendasLibrary.eventTags()
+    const linkedEventTag = this.agendasLibrary.prefTag('linkedEventTag')
 
     // create and show form
     const form = new Form()
     const tagNames = flattenedTags.map(t => t.name)
     form.addField(new Form.Field.Option('itemTag', 'Agenda Item Tag', flattenedTags, tagNames, itemTag, null))
+    form.addField(new Form.Field.Option('linkedEventTag', 'Linked Event Tag', flattenedTags, tagNames, linkedEventTag))
     form.addField(new Form.Field.Checkbox('addToNote', 'Add link to related tasks to notes', addToNote))
     form.addField(new Form.Field.MultipleOptions('eventTags', 'Event Tag(s)', flattenedTags, tagNames, eventTags))
     await form.show('Preferences: Agendas', 'OK')
 
     // save preferences
     syncedPrefs.write('itemTagID', form.values.itemTag.id.primaryKey)
+    syncedPrefs.write('linkedEventTagID', form.values.linkedEventTag.id.primaryKey)
     syncedPrefs.write('addToNote', form.values.addToNote)
     syncedPrefs.write('eventTagIDs', form.values.eventTags.map(tag => tag.id.primaryKey))
 
