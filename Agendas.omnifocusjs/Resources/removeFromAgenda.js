c6bf404b1f1301a8568f00e5008f94b5d184ee6c
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2022-02-16 18:43:39 +1100

    fix: :bug: allow 'remove from agenda' to be invoked on agenda items that are projects

--- Agendas.omnifocusjs/Resources/removeFromAgenda.js
+++ Agendas.omnifocusjs/Resources/removeFromAgenda.js
@@ -1,7 +1,9 @@
 /* global PlugIn Form */
 (() => {
   const action = new PlugIn.Action(async function (selection, sender) {
-    const events = selection.tasks.flatMap(task => this.agendasLibrary.getEvents(task))
+    const selected = Array.from(selection.tasks).concat(Array.from(selection.projects).map(i => i.task))
+
+    const events = selected.flatMap(task => this.agendasLibrary.getEvents(task))
     const uniqueEvents = Array.from(new Set(events))
 
     // create and show form
@@ -9,13 +11,12 @@
     form.addField(new Form.Field.MultipleOptions('eventsToRemove', 'Remove agenda items from the following events', uniqueEvents, uniqueEvents.map(e => e.name), uniqueEvents.length === 1 ? uniqueEvents : []))
     await form.show('Remove Event(s)', 'OK')
 
-    form.values.eventsToRemove.forEach(event => selection.tasks.forEach(async selected => await this.agendasLibrary.removeFromAgenda(event.id.primaryKey, selected.id.primaryKey)))
+    form.values.eventsToRemove.forEach(event => selected.forEach(async item => await this.agendasLibrary.removeFromAgenda(event.id.primaryKey, item.id.primaryKey)))
   })
 
   action.validate = function (selection, sender) {
-    if (selection.tasks.length === 0) return false
-
-    return selection.tasks.some(this.agendasLibrary.isItem)
+    const selected = Array.from(selection.tasks).concat(Array.from(selection.projects).map(i => i.task))
+    return selected.length > 0 && selected.some(this.agendasLibrary.isItem)
   }
 
   return action
