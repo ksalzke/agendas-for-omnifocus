b73e3063f5e8d8a61d180c90db15106576424a05
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2023-03-31 16:56:30 +1100

    fix: :bug: allow 'manage agenda items' to be run on events that are projects

--- Agendas.omnifocusjs/Resources/manageAgendaItems.js
+++ Agendas.omnifocusjs/Resources/manageAgendaItems.js
@@ -1,7 +1,7 @@
 /* global PlugIn Alert */
 (() => {
   const action = new PlugIn.Action(async function (selection, sender) {
-    const task = selection.tasks[0]
+    const task = selection.tasks[0] || selection.projects[0].task
     this.agendasLibrary.cleanUp()
     const items = this.agendasLibrary.getItems(task.id.primaryKey)
     if (items.length === 0) {
@@ -13,7 +13,8 @@
   })
 
   action.validate = function (selection, sender) {
-    return (selection.tasks.length === 1 && this.agendasLibrary.isEvent(selection.tasks[0]))
+    return (selection.tasks.length === 1 && this.agendasLibrary.isEvent(selection.tasks[0])) ||
+      (selection.projects.length === 1 && this.agendasLibrary.isEvent(selection.projects[0].task))
   }
 
   return action
